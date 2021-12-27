a7dcc15740cce97c772ab4d3d2a060b517a57710
Austin Ofor <kwadoskii@live.com>
2021-12-27 20:41:23 +0100

    temporary validating req.body.userId until auth is implemented.

--- middlewares/validateObjectId.js
+++ middlewares/validateObjectId.js
@@ -1,8 +1,11 @@
 import mongoose from "mongoose";
 
 export default (req, res, next) => {
-  if (!mongoose.Types.ObjectId.isValid(req.params.id))
-    return res.status(400).send({ status: "error", message: "Invalid ID" });
+  if (req.params.id && !mongoose.Types.ObjectId.isValid(req.params.id))
+    return res.status(400).send({ status: "error", message: "Invalid ID provided" });
+
+  if (req.body.userId && !mongoose.Types.ObjectId.isValid(req.body.userId))
+    return res.status(400).send({ status: "error", message: "Invalid ID provided" });
 
   next();
 };
